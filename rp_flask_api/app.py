ba67039c6a3c068b5527bd3b49bfe7f306a673f8
jacksonhmg <jacksonmowattgok@gmail.com>
2023-09-30 09:19:20 +0800

    created the first test api endpoint

--- rp_flask_api/app.py
+++ rp_flask_api/app.py
@@ -1,12 +1,10 @@
 # app.py
 
-from flask import render_template
+from flask import render_template # Remove: import Flask
 import connexion
 
 app = connexion.App(__name__, specification_dir="./")
 app.add_api("swagger.yml")
-
-app = Flask(__name__)
 
 @app.route("/")
 def home():
