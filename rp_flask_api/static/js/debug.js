743cd4087125d716753fe16fb44a992f4fa42df7
jacksonhmg <jacksonmowattgok@gmail.com>
2023-10-02 10:03:49 +0800

    getting rid of clear button

--- rp_flask_api/static/js/debug.js
+++ rp_flask_api/static/js/debug.js
@@ -4,20 +4,8 @@ export class DebugForm {
   constructor() {
     this.debugCard = document.querySelector(".debug-card");
     this.form = this.debugCard.querySelector(".debug-form");
-    this.clearButton = this.form.querySelector("button[data-action='clear']");
-    this.clearButton.addEventListener(
-      "click",
-      this.handleClearClick.bind(this)
-    );
     this.sendButton = this.form.querySelector("button[data-action='read']");
     this.sendButton.addEventListener("click", this.handleSendClick.bind(this));
-  }
-
-  handleClearClick(event) {
-    console.log("Clear button clicked")
-    event.preventDefault();
-    let code = this.debugCard.querySelector(".show-id");
-    code.innerText = "";
   }
 
   handleSendClick(event) {
